9d33f8b44e30800712a4c909d033f00ea914d0cd
JewellianaC <144458061+JewellianaC@users.noreply.github.com>
2024-03-14 09:57:32 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let button = document.querySelector('button');
+let box = document.getElementByld('changeMe');
+
+function changeColor(){
+    if (box.style.background == "red"){
+        box.style.background = "blue";
+    } else {
+        box.style.background = "red";
+    }
+}
